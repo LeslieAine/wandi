13c97ec75ca119e1bd78e604b2cf14fce43c9481
LeslieAine <aineleslie@gmail.com>
2023-12-20 13:31:02 +0300

    Run migrations for users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Define roles
+roles = %w(creator client admin)
+
+# Create roles
+roles.each do |role_name|
+  Role.find_or_create_by(name: role_name)
+end
